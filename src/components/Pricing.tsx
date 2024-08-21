b3f158260203eb23b363e10f6907ed54ad50d9a7
Nour <nourpow@gmail.com>
2024-08-21 05:26:07 +0100

    update  pricing and footer sections

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -49,43 +49,45 @@ function Pricing() {
             </Button>
           </div>
 
-          {/*pro plan */}
           {/* Pro plan */}
-          <div className="bg-white/50 rounded-lg border p-5 shadow-lg px-7 flex-1 flex flex-col justify-between">
-            <div>
-              {" "}
-              {/* Content container to push the button to the bottom */}
-              <div className="flex items-end gap-3">
-                <p className="text-4xl font-bold">15</p>
-                <p className="text-gray-600 text-md">/month</p>
-              </div>
-              <p className="text-3xl font-bold pt-3">Pro Plan</p>
-              <p className="py-3">
-                Perfect for growing businesses needing advanced features
-              </p>
-              <hr />
-              <div className="py-3 flex gap-2 items-center">
-                <BadgeCheck fill="#a94cf7" className="h-5 w-5" />
-                <p>Up to 100 forms per month</p>
-              </div>
-              <div className="py-3 flex gap-2 items-center">
-                <BadgeCheck fill="#a94cf7" className="h-5 w-5" />
-                <p>Data export in CSV and Excel formats</p>
-              </div>
-              <div className="py-3 flex gap-2 items-center">
-                <BadgeCheck fill="#a94cf7" className="h-5 w-5" />
-                <p>Conditional logic and branching</p>
-              </div>
-              <div className="py-3 flex gap-2 items-center">
-                <BadgeCheck fill="#a94cf7" className="h-5 w-5" />
-                <p>Basic analytics and reporting</p>
-              </div>
+          <div className="flex flex-col ">
+            <div className="bg-[#a94cf7] rounded-t-lg text-center text-lg font-semibold text-white  py-1">
+              Popular
             </div>
-            <Button className="bg-[#a94cf7] w-full hover:bg-purple-700 font-semibold mt-4 ">
-              Select Plan
-            </Button>
+            <div className="bg-white/50 rounded-b-lg border p-5 shadow-lg px-7 flex-1 flex flex-col justify-between py-7">
+              <div>
+                {/* Content container to push the button to the bottom */}
+                <div className="flex items-end gap-3">
+                  <p className="text-4xl font-bold">15</p>
+                  <p className="text-gray-600 text-md">/month</p>
+                </div>
+                <p className="text-3xl font-bold pt-3">Pro Plan</p>
+                <p className="py-3">
+                  Perfect for growing businesses needing advanced features
+                </p>
+                <hr />
+                <div className="py-3 flex gap-2 items-center">
+                  <BadgeCheck fill="#a94cf7" className="h-5 w-5" />
+                  <p>Up to 100 forms per month</p>
+                </div>
+                <div className="py-3 flex gap-2 items-center">
+                  <BadgeCheck fill="#a94cf7" className="h-5 w-5" />
+                  <p>Data export in CSV and Excel formats</p>
+                </div>
+                <div className="py-3 flex gap-2 items-center">
+                  <BadgeCheck fill="#a94cf7" className="h-5 w-5" />
+                  <p>Conditional logic and branching</p>
+                </div>
+                <div className="py-3 flex gap-2 items-center">
+                  <BadgeCheck fill="#a94cf7" className="h-5 w-5" />
+                  <p>Basic analytics and reporting</p>
+                </div>
+              </div>
+              <Button className="bg-[#a94cf7] w-full hover:bg-purple-700 font-semibold mt-4 ">
+                Select Plan
+              </Button>
+            </div>
           </div>
-
           {/*Enterprise plan */}
           <div className="bg-white/50 rounded-lg border p-5 shadow-lg px-7 flex-1 flex flex-col justify-between">
             <div>
